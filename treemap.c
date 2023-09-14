ad52f42ac4daa0dd4ba1e3320e7db6086f5df32e
Replit user <>
2023-09-14 02:10:12 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -74,9 +74,11 @@ void eraseTreeMap(TreeMap * tree, void* key){
 Pair * searchTreeMap(TreeMap * tree, void* key) {
   TreeNode* current = tree->lower_than
   while(current == NULL){
-    if(){
+    if(isequal(tree,key,current->key)){
+      tree->lower_than = lower_than;  
       return;
-    }else if(){
+    }else if(tree->lower_than(key, current->pair->key)){
+      current = current-left;
       return;
     }else{
       return;
