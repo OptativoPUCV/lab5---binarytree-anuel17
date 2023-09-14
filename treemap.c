fda7a094c66075d196fe87c55b14fe360244d40b
Replit user <>
2023-09-14 02:13:20 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -78,9 +78,10 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
       tree->current = current;  
       return current->pair;
     }else if(tree->lower_than(key, current->pair->key)){
-      current= current->left;
-    }else{
       current= current->right;
+    }else{
+      
+      current= current->left;
     }
   }
   return NULL;
